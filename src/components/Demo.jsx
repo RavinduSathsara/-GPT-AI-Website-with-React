3890a9abbb9e736cb78d5a3cf7f2878cd7cb987b
RavinduSathsara <rsathsara330@gmail.com>
2023-04-23 09:03:34 +0530

    add copy function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,7 +7,7 @@ const Demo = () => {
 
   const [allArticles, setAllArticles] = useState([]);
 
-  console.log(allArticles);
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -32,6 +32,12 @@ const Demo = () => {
       setAllArticles(updatedAllArticles);
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -75,9 +81,9 @@ const Demo = () => {
               }}
               className="link_card"
             >
-              <div className="copy_btn">
+              <div className="copy_btn" onClick={() => handleCopy(item?.url)}>
                 <img
-                  src={copy}
+                  src={copied === item?.url ? tick : copy}
                   alt="copy_icon"
                   className="w-[40%] h-[40%] object-contain"
                 />
